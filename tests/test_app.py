1ea8a67fb63604559744d8a1f9fde06c3676e348
Allan Bruno <allanbrunorj@gmail.com>
2020-09-25 12:02:10 -0300

    Rota inicial de login

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,3 +3,12 @@ from flask import Flask
 
 def test_create_app_deve_retornar_um_app_flask():
 	assert isinstance(create_app(), Flask)
+
+def test_login_deve_retornar_sucesso():
+	app = create_app()
+	app.config['TESTING'] = True
+
+	with app.test_client() as client:
+		response = client.get('/login') # 1
+
+	assert response.status_code == 200 # 2
